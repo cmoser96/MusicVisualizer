392fffbcb2fcb49b5d6f2bb7d291c2bb6d779d5c
cmoser96 <cm898946@gmail.com>
2019-08-17 03:35:41 -0400

    condensing down to 1 program

--- Opengl/main.cpp
+++ Opengl/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <string.h>
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <pulse/simple.h>
+#include <fftw3.h>
+
 #include "main.h"
-#define SAMPLE_RATE 192000
-#define FRAME_RATE 30
-#define BUFFER_SIZE SAMPLE_RATE/FRAME_RATE
 
 int main(int argc, char* args[])
 {
+    lower = 0;
+    upper = 1400;
     ss.format = PA_SAMPLE_U8;
     ss.channels = 1;
     ss.rate = SAMPLE_RATE;
@@ -36,19 +38,24 @@ int main(int argc, char* args[])
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
+    in = (double *) fftw_malloc(sizeof(double)*N);
+    out = (double (*)[2])fftw_malloc(sizeof(fftw_complex)*nc);
+
     glClearColor(0.f, 0.f, 0.f, 1.f);
     glutTimerFunc(1000/60, mainLoop, 0);
 
     glutMainLoop();
+    fftw_free(in);
+    fftw_free(out);
     return 0;
 }
 
 void mainLoop(int val){
-    render();
+    renderFFT();
     glutTimerFunc(1000/1800, mainLoop, val);
 }
 
-void render(){
+void renderBouncy(){
     glClear(GL_COLOR_BUFFER_BIT);
 
     uint8_t buf[BUFFER_SIZE];
@@ -92,4 +99,50 @@ void render(){
     glEnd();
 
     glutSwapBuffers();
+}
+
+void renderWaveform(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin (GL_LINES);
+    glColor3f(1.0f, 0.6f, 0.0f);
+    glLineWidth(2.f);
+    static uint8_t buf[DOUBLE_BUFFER];
+    memcpy(buf, (buf+BUFFER_SIZE), BUFFER_SIZE);
+    pa_simple_read(s, (buf+BUFFER_SIZE), BUFFER_SIZE, NULL);
+    for(int i = 0; i < DOUBLE_BUFFER; i ++){
+        glVertex2f(-1.f + 2*float(i)/sizeof(buf), float(int(buf[i] - 128))/364);
+    }
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+
+void renderFFT(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    pa_simple_read(s, buf, BUFFER_SIZE, NULL);
+    for (int i = 0; i < N; i++){
+        in[i] = abs(buf[i] - 128);
+    }
+
+    p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE);
+    fftw_execute(p);
+
+    for(int i = lower; i < upper; i ++){
+        glBegin (GL_LINES);
+        glColor3f(1.0f, 0.6f, 0.0f);
+        glLineWidth(3.f);
+        glVertex2f(-1.f + 2*float(i)/(upper+lower), -1.f);
+        glVertex2f(-1.f + 2*float(i)/(upper+lower), (float)abs(out[i][0])/10000-1);
+        glEnd();
+    }
+
+    fftw_destroy_plan(p);
+
+    glutSwapBuffers();
+}
+
+void bucket(){
 }
