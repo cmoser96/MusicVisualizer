147b92c5d342dac1ed064e354eb11f5760322781
cmoser96 <cm898946@gmail.com>
2019-09-02 20:11:56 -0400

    started to clean up

--- Opengl/get_sinks.cpp
+++ Opengl/get_sinks.cpp
@@ -5,87 +5,63 @@ pa_mainloop *pa_ml;
 pa_mainloop_api *pa_mlapi;
 pa_context *c;
 pa_operation *op;
+pa_context_state_t state = PA_CONTEXT_UNCONNECTED;
+
+struct pa_device{
+    pa_sink_state_t state;
+    std::string name;
+    pa_device(){
+        state = PA_SINK_INVALID_STATE;
+        name = "";
+    }
+};
 
 void get_sink_info_callback(pa_context *c, const pa_sink_info *i, int is_last, void *userdata){
+    pa_device *d = reinterpret_cast<pa_device*>(userdata);
+
     if(is_last>0){
         return;
     }
+
     if(i->state == PA_SINK_RUNNING){
         std::cout << i->name << std::endl;
     }
 }
 
 void pa_state_cb(pa_context *c, void *userdata) {
-        pa_context_state_t state;
-        int *pa_ready = (int*) userdata;
-
-        state = pa_context_get_state(c);
-
-        switch  (state) {
-                // There are just here for reference
-                case PA_CONTEXT_UNCONNECTED:
-                    std::cout << "PA_CONTEXT_UNCONNECTED" << std::endl;
-                    break;
-                case PA_CONTEXT_CONNECTING:
-                    std::cout << "PA_CONTEXT_CONNECTING" << std::endl;
-                    break;
-                case PA_CONTEXT_AUTHORIZING:
-                    std::cout << "PA_CONTEXT_AUTHORIZING" << std::endl;
-                    break;
-                case PA_CONTEXT_SETTING_NAME:
-                    std::cout << "PA_CONTEXT_SETTING_NAME" << std::endl;
-                    break;
-                default:
-                        break;
-                case PA_CONTEXT_FAILED:
-                    std::cout << "PA_CONTEXT_FAILED" << std::endl;
-                    break;
-                case PA_CONTEXT_TERMINATED:
-                        std::cout << "PA_CONTEXT_TERMINATED" << std::endl;
-                        *pa_ready = 2;
-                        break;
-                case PA_CONTEXT_READY:
-                        *pa_ready = 1;
-                        std::cout << "PA_CONTEXT_READY" << std::endl;
-                        break;
-        }
+    pa_context_state_t *s = reinterpret_cast<pa_context_state_t*>(userdata);
+    *s = pa_context_get_state(c);
 }
 
 int main(int argc, char* args[])
 {
+    pa_device list[8];
+
     pa_ml = pa_mainloop_new();
     pa_mlapi = pa_mainloop_get_api(pa_ml);
     c = pa_context_new(pa_mlapi, "test");
-    int pa_ready = 0;
     pa_context_connect(c, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
-    pa_context_set_state_callback(c, pa_state_cb, &pa_ready);
+    pa_context_set_state_callback(c, pa_state_cb, &state);
 
-    for(;;){
-        if (pa_ready == 0) {
-            pa_mainloop_iterate(pa_ml, 1, NULL);
-            continue;
-        }
-        if (pa_ready == 2) {
-            pa_context_disconnect(c);
-            pa_context_unref(c);
-            pa_mainloop_free(pa_ml);
-            return -1;
-        }
-        if (pa_ready == 1) {
-            break;
+    while(state != PA_CONTEXT_READY){
+        switch (state) {
+            case PA_CONTEXT_FAILED:
+            case PA_CONTEXT_TERMINATED:
+                pa_context_disconnect(c);
+                pa_context_unref(c);
+                pa_mainloop_free(pa_ml);
+                return -1;
+            default:
+                pa_mainloop_iterate(pa_ml, 1, NULL);
+                break;
         }
     }
-    std::cout << "starting op" << std::endl;
 
-    op = pa_context_get_sink_info_list(c, get_sink_info_callback, NULL);
+    std::cout << "starting op" << std::endl;
+    op = pa_context_get_sink_info_list(c, get_sink_info_callback, list);
 
 
     while(pa_operation_get_state(op) != PA_OPERATION_DONE){
-        if (pa_ready == 0) {
-            pa_mainloop_iterate(pa_ml, 1, NULL);
-            continue;
-        }
-
         pa_mainloop_iterate(pa_ml, 1, NULL);
     }
 
