7c568f6a3f2259ef4d52582a79182e6029b7b520
cmoser96 <cm898946@gmail.com>
2019-09-09 19:03:41 -0400

    converting tabs to spaces

--- src/keyboard_input.cpp
+++ src/keyboard_input.cpp
@@ -2,25 +2,25 @@
 
 
 void processNormalKeys(unsigned char key, int x, int y){
-	switch(key){
-		case 27:
-			glutReshapeWindow(1800,950);
-			break;
-		case 'f':
-			glutFullScreenToggle();
-			break;
-		case 's':
-			break;
-		case 'x':
-			glutLeaveMainLoop();
-			break;
-		default:
-			if(key >= '0' && key <='9'){
+    switch(key){
+        case 27:
+            glutReshapeWindow(1800,950);
+            break;
+        case 'f':
+            glutFullScreenToggle();
+            break;
+        case 's':
+            break;
+        case 'x':
+            glutLeaveMainLoop();
+            break;
+        default:
+            if(key >= '0' && key <='9'){
 
-			}
-	}
+            }
+    }
 }
 
 void processSpecialKeys(int key, int x, int y){
-
+    //TODO
 }
